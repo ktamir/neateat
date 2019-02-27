6b3ea0e902e673f76e1059e8eeec781714a18b95
Tamir Kfir <tamir.kfir@wework.com>
2019-02-27 17:26:44 +0200

    Added restaurant model, controller, routes and tests

---
+++ server/spec/factories/restaurants.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id                :bigint(8)        not null, primary key
+#  name              :string
+#  cuisine           :string
+#  rating            :integer
+#  accepts_10bis     :boolean
+#  address           :string
+#  max_delivery_time :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
+FactoryBot.define do
+  factory :restaurant do
+    name { Faker::Restaurant.name }
+    cuisine { Faker::Restaurant.type }
+    rating { Faker::Number.between(1, 3) }
+    accepts_10bis { Faker::Boolean }
+    address { Faker::Address }
+    max_delivery_time { Faker::Number.between(30, 120) }
+  end
+end
