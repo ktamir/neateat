10ef3bfeb89057460a38f5684df80e4a041d487e
Tamir Kfir <tamir.kfir@wework.com>
2019-02-28 11:27:55 +0200

    Spacing

--- server/spec/routing/restaurants_routing_spec.rb
+++ server/spec/routing/restaurants_routing_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe RestaurantsController, type: :routing do
       expect(get: '/restaurants/1').to route_to('restaurants#show', id: '1')
     end
 
-
     it 'routes to #create' do
       expect(post: '/restaurants').to route_to('restaurants#create')
     end
