fcd3cc57047e87126f3c4be38e91caadfbbe71c2
Tamir Kfir <tamir.kfir@wework.com>
2019-03-05 16:44:38 +0200

    Added db seed and cors

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+
+20.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    cuisine: Faker::Restaurant.type,
+    rating: Faker::Number.between(1, 3),
+    accepts_10bis: Faker::Boolean,
+    address: Faker::Address.street_address,
+    max_delivery_time: Faker::Number.between(30, 120)
+  )
+end
