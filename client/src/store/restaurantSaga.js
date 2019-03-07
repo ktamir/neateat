b8898e4f5f90a31ba69528abf0d3542b29214818
Tamir Kfir <tamir.kfir@wework.com>
2019-03-07 14:58:50 +0200

    Added type for

--- client/src/store/restaurantSaga.js
+++ client/src/store/restaurantSaga.js
@@ -1,11 +1,12 @@
 // @flow
 
 import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest, type PutEffect } from 'redux-saga/effects';
 import { type Saga } from 'redux-saga';
 import { API_URL } from '../consts';
+import { Action } from 'redux';
 
-function *fetchRestaurants() {
+function *fetchRestaurants() : PutEffect<Action> {
   try {
     const response = yield axios.get(`${API_URL}/restaurants`);
     yield put({ type: 'FETCH_RESTAURANTS_SUCCESS', data: response.data });
