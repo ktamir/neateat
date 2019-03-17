bde2de0d6b6101c5bda0e9c38a62011888685341
Tamir Kfir <tamir.kfir@wework.com>
2019-03-17 14:26:30 +0200

    Fix tests

--- client/src/test/RestaurantList.test.jsx
+++ client/src/test/RestaurantList.test.jsx
@@ -12,19 +12,19 @@ describe('RestaurantList', () => {
 
   it('should trigger fetch request', () => {
     mount(<RestaurantList restaurants={[]} fetchRestaurants={fetchRestaurants}
-                          fetchRestaurantsInProgress={false} />);
+      fetchRestaurantsInProgress={false} fetchRestaurantsError={null} />);
     expect(fetchRestaurants).toHaveBeenCalledTimes(1);
   });
 
   it('should render restaurants correctly', () => {
     let restaurantCard = shallow(<RestaurantList restaurants={mockRestaurants} fetchRestaurants={fetchRestaurants}
-                                                 fetchRestaurantsInProgress={false} />);
+      fetchRestaurantsInProgress={false} fetchRestaurantsError={null} />);
     expect(restaurantCard).toMatchSnapshot();
   });
 
   it('should render loading correctly', () => {
     let restaurantCard = shallow(<RestaurantList restaurants={[]} fetchRestaurants={fetchRestaurants}
-                                                 fetchRestaurantsInProgress={true} />);
+      fetchRestaurantsInProgress={true} fetchRestaurantsError={null} />);
     expect(restaurantCard).toMatchSnapshot();
   });
 });
